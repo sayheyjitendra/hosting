19d5fd506ed95da17c5931a22191c10358803162
Jitendra <jitendraks1803@gmail.com>
2021-12-08 21:24:55 +0530

    changes

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 countryp = document.getElementById("cc");
 
-
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
@@ -10,52 +9,52 @@ xhttp.onreadystatechange = function () {
     country_code = country_code[1].split("tls=");
     country_code = country_code[0];
 
- 
     if (country_code == "IN") {
-        countryp.innerHTML = `<img src = "images/india.png">`
-        console.log(country_code);
+      countryp.innerHTML = `<img src = "images/india.png">`;
+      console.log(country_code);
+    } else if (country_code == "US") {
+      countryp.innerHTML = `<img src = "images/united-states.png">`;
+      console.log(country_code);
     }
-
-    
-        
-    else if (country_code == "US") {
-        countryp.innerHTML = `<img src = "images/united-states.png">`
-        console.log(country_code);
-    }
-
-   
   }
 };
 
-//OPEN HTTP Request
-// xhttp.open("GET", "https://www.cloudflare.com/cdn-cgi/trace", true);
-// xhttp.send();
-
-
+// OPEN HTTP Request
+xhttp.open("GET", "https://www.cloudflare.com/cdn-cgi/trace", true);
+xhttp.send();
 
 // For toggling
 
-
 const shared = document.getElementById("shared");
+const sharedPlan = document.querySelector(".sharedplan");
 const cloud = document.getElementById("cloud");
-
+const cloudPlan = document.querySelector(".cloudplan");
 
 cloud.addEventListener("click", function () {
-  shared.classList.toggle("active");
-  cloud.classList.toggle("active");
+  shared.classList.remove("active");
+  cloud.classList.add("active");
+  sharedPlan.classList.add("hidden");
+  cloudPlan.classList.remove("hidden");
 });
 
 shared.addEventListener("click", function () {
   shared.classList.add("active");
-  
+
   cloud.classList.remove("active");
+
+  sharedPlan.classList.remove("hidden");
+  cloudPlan.classList.add("hidden");
 });
 
+const toggle = document.getElementById("toggle");
+const flexy = document.getElementById("flexy");
 
-const toggle = document.getElementById('toggle');
-const flexy = document.getElementById('flexy');
-
-toggle.addEventListener('change', e => {
-	flexy.classList.toggle('show-monthly');
+toggle.addEventListener("change", (e) => {
+  flexy.classList.toggle("show-monthly");
 });
 
+const lannually = document.querySelector("span.annually");
+
+if (1 == 1) {
+  lannually.innerHTML = "hola";
+}
